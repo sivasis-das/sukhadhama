776ecbd4c90bdd8d6697a093e8f2f27b2693a1c5
sivasis das <sivasisd7@gmail.com>
2024-03-02 18:00:26 +0530

    connect to data base

--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+
+dotenv.config()
 
 const app = express();
 const port = 3000;
 
+mongoose.connect(process.env.MONGODB).then(()=>console.log("connected to mongodb")).catch(error=>console.log(error));
 
-app.listen(port, ()=>{
-    console.log(`server listening at ${port}`);
-})
+app.listen(port, () => {
+  console.log(`server listening at ${port}`);
+});
