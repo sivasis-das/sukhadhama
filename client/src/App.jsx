f6c5ae4d62e50ae91d808d5490efaf19862692ae
sivasis das <sivasisd7@gmail.com>
2024-03-02 10:36:16 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  let a = 2;
+  let b = 3;
+  let c = a+b;
+  console.log(c); 
+
+  return (
+    <div className="text-orange-500 bg-yellow-300 text-center uppercase text-3xl font-bold">
+      Jai jagannath
+    </div>
+  );
+}
+
+export default App;
