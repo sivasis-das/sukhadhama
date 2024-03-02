2357bed8963aa6717a5a2296fd77e654f21b9c5b
sivasis das <sivasisd7@gmail.com>
2024-03-02 20:47:47 +0530

    created test api rout , created model, controller and route file structure

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import userRouter from "./routes/user.route.js"
 
 dotenv.config()
 
@@ -9,6 +9,8 @@ const app = express();
 const port = 3000;
 
 mongoose.connect(process.env.MONGODB).then(()=>console.log("connected to mongodb")).catch(error=>console.log(error));
+
+app.use('/api/user', userRouter)
 
 app.listen(port, () => {
   console.log(`server listening at ${port}`);
