b7dc541e48e30f6266f2492f1642cda12b410f23
sivasis das <sivasisd7@gmail.com>
2024-03-02 11:14:06 +0530

    routes, pages created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,31 @@
 import React from "react";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
 
 function App() {
-  let a = 2;
-  let b = 3;
-  let c = a+b;
-  console.log(c); 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route path="" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="profile" element={<Profile />} />
 
-  return (
-    <div className="text-orange-500 bg-yellow-300 text-center uppercase text-3xl font-bold">
-      Jai jagannath
-    </div>
+        <Route path="sign-up" element={<SignUp />} />
+        <Route path="sign-in" element={<Signin />} />
+      </Route>
+    )
   );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
