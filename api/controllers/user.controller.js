18b8deac2101e72f565ef86a851325055d4451aa
sivasis das <sivasisd7@gmail.com>
2024-03-17 10:36:23 +0530

    created get user listings API route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req, res) => {
   res.json({
@@ -9,6 +10,7 @@ export const test = (req, res) => {
 };
 
 export const updateUserInfo = async (req, res, next) => {
+  //req.user.id comes from the verifyUser.js---
   if (req.user.id !== req.params.id)
     return next(errorHandler(401, "You can only update your own account!"));
   try {
@@ -45,3 +47,14 @@ export const deleteUser = async(req, res, next)=>{
   }
   
 }
+
+
+export const getUserListings = async(req, res, next)=>{
+  if(req.user.id !== req.params.id) return next(errorHandler(401, 'You can only view your own listings!'));
+  try {
+    const listings = await Listing.find({userRef: req.params.id})
+    res.status(200).json(listings)
+  } catch (error) {
+    next(error)
+  }
+} 
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUser, test, updateUserInfo } from "../controllers/user.controller.js";
+import { deleteUser, getUserListings, test, updateUserInfo } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/test', test);
 router.post('/update/:id',verifyToken, updateUserInfo)
 router.delete('/delete/:id',verifyToken, deleteUser)
+router.get('/listings/:id', verifyToken, getUserListings)
 
 export default router
