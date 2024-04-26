2b44887655d59200314988e6db5aa09e9fd4cb47
sivasis das <sivasisd7@gmail.com>
2024-04-26 12:04:05 +0530

    added pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
 import listingRouter from "./routes/listing.route.js"
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config()
 
@@ -15,12 +16,21 @@ const port = 3000;
 
 mongoose.connect(process.env.MONGODB).then(()=>console.log("connected to mongodb")).catch(error=>console.log(error));
 
+const __dirname = path.resolve();
+
 app.use(express.json())
 app.use(cookieParser())
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+// *-means any address except the above three router address
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 
 // express.js error handler 
